a79714060370c471104d9293ea74b7fefc1b69cd
SAE1\1030802 <1030802@student.sae.edu.au>
2024-06-18 10:50:12 +1000

    Innitial commit of Pixel attack

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float playerSpeed;
+    [SerializeField] float playerRot;
+    //Vector2 movement;
+
+
+     Rigidbody2D rb2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+        //movement = new Vector2(playerSpeed * 100, 0);
+
+        playerRot = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
+        transform.Rotate(0, 0, -playerRot * 100);
+
+        playerSpeed = Input.GetAxisRaw("Vertical") * Time.deltaTime;
+        rb2D.AddForce(transform.up * playerSpeed * 100, ForceMode2D.Force); 
+        //rb2D.velocity = movement;
+
+
+
+    }
+}
